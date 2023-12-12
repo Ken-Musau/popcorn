dcee9ef619bb564c8e36e91fe89661f9e388b140
Kennedy Musau <kennedy.musauu@gmail.com>
2023-12-12 13:09:31 +0300

    update app component

--- src/index.js
+++ src/index.js
@@ -1,29 +1,35 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-// import './index.css';
-// import App from './App';
-
-import StarRating from "./StarRating";
-
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-  return (
-    <>
-      <StarRating color="blue" onsetRating={setMovieRating} />
-      <p>This movie was rated {movieRating} stars</p>
-    </>
-  );
-}
+import "./index.css";
+import App from "./App";
+// import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
-      maxRating={5}
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-      defaultRating={3}
-    />
-    <Test />
+    <App />
   </React.StrictMode>
 );
+
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
+//   return (
+//     <>
+//       <StarRating color="blue" onsetRating={setMovieRating} />
+//       <p>This movie was rated {movieRating} stars</p>
+//     </>
+//   );
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     {/* <App /> */}
+//     <StarRating
+//       maxRating={5}
+//       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+//       defaultRating={3}
+//     />
+//     <Test />
+//   </React.StrictMode>
+// );
